da4dcceec0fe7465cd005f22ef8410db9dd6936f
chrismbirmingham <cmbirmingham19@gmail.com>
2022-03-16 13:29:55 -0700

    reorg

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
-from data.data_utils import Data
+from data import Data
 from tqdm import tqdm
 
-"""common_kwargs = dict(path2data = './data/PATS',
-                     speaker = ['oliver'],
-                     modalities = ['pose/data', 'audio/log_mel_512', 'text/bert'],
+common_kwargs = dict(path2data = '../PATS/data',
+                     speaker = ['bee'],
+                     modalities = ['pose/data',],
                      fs_new = [15, 15, 15],
                      batch_size = 4,
                      window_hop = 5)
 
-data = Data(**common_kwargs)"""
+data = Data(**common_kwargs)
 
-from data.data_utils import HDF5
+for batch in data.train:
+    pose = batch['pose/data']
+    print(pose.shape)
+    print("something")
 
-h5 = HDF5.h5_open('./data/PATS/processed/oliver/cmu0000006502.h5', 'r')
-print(h5.keys())
-for key in h5.keys():
-    print('{}: {}'.format(key, h5[key].keys()))
-h5.close()
+    break
